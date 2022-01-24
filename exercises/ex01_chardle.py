03b83de130ad5a7b6397e3b521c1278353ccba3c
Amulya Katragadda <you@example.com>
2022-01-23 22:50:47 -0500

    Progress on Exercise 1

---
+++ exercises/ex01_chardle.py
@@ -0,0 +1,44 @@
+"""EX01 - Chardle - A cute step toward Wordle."""
+
+__author__ = 730407531
+
+word: str = input("Enter a 5-character word: ")
+if len(word) <5:
+    print("Error: Word must contain 5 characters")
+    exit()
+if len(word) >5:
+    print("Error: Word must contain 5 characters")
+    exit()
+character: str = input("Enter a single character: ")
+if len(character) >1:
+    print("Error: Character must be a single character.")
+    exit()
+if len(character) <1:
+    print("Error: Character must be a single character.")
+    exit()
+counter: int = 0
+print("Searching for " + character + " in " + word)
+
+if word[0] == character:
+    print(character + " is found at index 0")
+    counter = counter + 1
+if word[1] == character:
+    print(character + " is found at index 1")
+    counter = counter + 1
+if word[2] == character:
+    print(character + " is found at index 2")
+    counter = counter + 1
+if word[3] == character:
+    print(character + " is found at index 3")
+    counter = counter + 1
+if word[4] == character:
+    print(character + " is found at index 4")
+    counter = counter + 1
+
+if counter == 0:
+    print("No instances of " + character + " found in " + word)
+if counter == 1:
+    print("1 instance of " + character + " found in " + word)
+if counter == 2:
+    print("2 instances of " + character + " found in " + word)
+
