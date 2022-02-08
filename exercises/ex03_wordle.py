dde5921da56c9a10772263624f6013a221131e37
Amulya Katragadda <you@example.com>
2022-02-07 21:38:31 -0500

    progress on exercise 3

---
+++ exercises/ex03_wordle.py
@@ -0,0 +1,55 @@
+"""Wordle."""
+
+__author__ = "730407531"
+
+from xmlrpc.client import boolean
+
+def contains_char(word: str, char: str) -> bool:
+    """Given a guessed word, returns a correct character contained."""
+    assert len(char) == 1
+    i: int = 0
+    while i < len(word):
+        if word[i] == char:
+            return True
+        i = i + 1
+    return False
+    
+def emojified(guess: str, secret: str) -> str:
+    """Given a guess and a secret word, return a string of emojis whose color codifies the same as EX02."""
+    assert len(guess) == len(secret)
+    i: int = 0
+    counter: int = 0
+    emoji_result: str = ""
+    WHITE_BOX: str = "\U00002B1C"
+    GREEN_BOX: str = "\U0001F7E9"
+    YELLOW_BOX: str = "\U0001F7E8"
+    wrong_spot: bool = False
+    while i < len(guess):
+        if guess[i] == secret[i]:
+            emoji_result = emoji_result + GREEN_BOX
+        else:
+            wrong_spot = True
+            while counter < len(secret):
+                if secret[counter] == guess[i]:
+                    emoji_result = emoji_result + YELLOW_BOX
+                    counter = 6
+                    wrong_spot = True
+                else:
+                    counter = counter + 1
+                    wrong_spot = False    
+            if wrong_spot == False:
+                emoji_result = emoji_result + WHITE_BOX
+        counter = 0
+        i = i + 1
+    return emoji_result 
+
+def input_guess(exp_len: int) -> str:
+    """Given an integer "expected length" of a guess, will prompt user for a guess until they guess a word of the appropriate length."""
+    wordle_guess: str = input(f"Enter a {exp_len} character word: ")
+    while len(wordle_guess) != exp_len:
+        if len(wordle_guess) < exp_len:
+            wordle_guess = input(f"That wasn't {exp_len} chars! Try again: ")
+        if len(wordle_guess) > exp_len:
+            wordle_guess = input(f"That wasn't {exp_len} chars! Try again: ")
+    if len(wordle_guess) == exp_len:
+        return wordle_guess
